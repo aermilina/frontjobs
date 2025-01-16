5ab19e536eeab5676869546a05a6403dbfeb01a5
Анна Ермилина <annaermilina@MacBook-Air-Anna-2.local>
2025-01-16 15:11:09 +0100

    render port

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import logging
 import os
-import asyncio
 from src.scheduler import start_scheduler  # Импортируем функцию из scheduler
 
 # Запуск асинхронной функции через asyncio
 async def main():
     # Логируем запуск бота
     logging.info("Запуск бота и планировщика.")
-    PORT = int(os.environ.get("PORT", 5000))
+    PORT = int(os.environ.get("PORT", 5000))  # Получаем порт из переменной окружения Render
     await start_scheduler()  # Ждем завершения работы планировщика
 
 # Запуск главной асинхронной функции
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    asyncio.run(main())  # Запуск асинхронной функции main()
-
+    
+    # Используем uvicorn для запуска приложения
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=PORT)  # Подключение к порту из переменной окружения
